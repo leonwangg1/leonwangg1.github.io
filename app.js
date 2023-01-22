7ae9ac312e1683130c1b80fa15c4897cdd5040c0
Leon <itsleonwang1@gmail.com>
2023-01-22 16:11:10 +1100

    Audio test

--- app.js
+++ app.js
@@ -28,10 +28,10 @@ document.addEventListener("DOMContentLoaded", () => {
       easing: "easeInBounce",
       scale: 2.8,
       complete: () => {
-        anime({
-          targets: ".namecenterpage .square",
-          animation: "pulse 1s infinite",
-        });
+        // anime({
+        //   targets: ".namecenterpage .square",
+        //   animation: "pulse 1s infinite",
+        // });
       },
     });
   });
@@ -42,7 +42,15 @@ document.addEventListener("DOMContentLoaded", () => {
       borderRadius: ["50%", "0%"],
       easing: "easeOutBounce",
       scale: 1,
-      animation: "none",
+      // animation: "none",
     });
+  });
+
+  // Adjust animation speed to bpm of music
+  var buffer = new Tone.Buffer("public/music.mp3", function () {
+    var bpm = buffer.context.lookup.bpm;
+    console.log("BPM: ", bpm);
+    var player = new Tone.Player(buffer).toMaster();
+    player.start();
   });
 });
